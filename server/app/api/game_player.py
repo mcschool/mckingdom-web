2bf962942bfe3a9ac9f7cf0c72a358c961fffee3
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-03 15:25:32 +0900

    test3

--- server/app/api/game_player.py
+++ server/app/api/game_player.py
@@ -10,7 +10,7 @@ def get_players():
 
 @app.route("/api/game/players", methods=['PATCH'])
 def patch_players():
-    return "byebye"
+    return "bye bye"
 
 
 @app.route("/api/game/players", methods=['POST'])
@@ -26,3 +26,35 @@ def put_players():
 @app.route("/api/game/players", methods=['DELETE'])
 def delete_players():
     return "yes"
+
+
+@app.route("/api/game/players/<uuid>", methods=['GET'])
+def get_player(uuid=None):
+    print("=============")
+    print(uuid)
+    print("=============")
+    return "GET: /api/game/players/<uuid>"
+
+
+@app.route("/api/game/players/<uuid>/password", methods=['PATCH'])
+def patch_password(uuid=None):
+    print("=============")
+    print(uuid + " change_password")
+    print("=============")
+    return "GET: /api/game/players/<uuid>/password"
+
+
+@app.route("/api/game/players/<uuid>/email", methods=['PATCH'])
+def patch_email(uuid=None):
+    print("=============")
+    print(uuid + " change_email")
+    print("=============")
+    return "GET: /api/game/players/<uuid>/email"
+
+
+@app.route("/api/game/players/<uuid>", methods=['DELETE'])
+def delete_player(uuid=None):
+    print("=============")
+    print(uuid + " delete_player")
+    print("=============")
+    return "GET: /api/game/players/<uuid>"
