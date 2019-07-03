2246f7d6f608dd237328fa7ca84fe5cd6b0c0065
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-03 16:40:23 +0900

    test5

--- server/app/api/game_player.py
+++ server/app/api/game_player.py
@@ -15,8 +15,25 @@ def patch_players():
     if data['uuid'] is None:
         print("error: no uuid")
     else :
-        print("uuid: " + data['uuid'])
-        print("name: " + data['name'])
+        p = g.session.query(Player).filter(
+            Player.uuid == data['uuid']
+        ).first()
+        if p is None:
+            print("いないから登録")
+            player = Player()
+            player.uuid = data['uuid']
+            player.name = data['name']
+            g.session.add(player)
+            g.session.commit()
+        else:
+            p.login_count = p.login_count + 1
+            g.session.commit()
+            if p.name == data['name']:
+                pass
+            else:
+                p.name = p.name = data['name']
+                g.session.commit()
+
     return "bye bye"
 
 
