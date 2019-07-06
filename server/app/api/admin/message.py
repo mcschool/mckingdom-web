27d32aa7849f9b46c9a2ca4a12059009c2b0d32a
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-06 14:21:57 +0900

    final

--- server/app/api/admin/message.py
+++ server/app/api/admin/message.py
@@ -4,21 +4,26 @@ from ...models.message import Message
 
 app = Blueprint('admin_message', __name__)
 
+
 @app.route("/api/admin/messages", methods=['GET'])
 def get_messages():
     return "hello messages"
+
 
 @app.route("/api/admin/messages/:id", methods=['GET'])
 def get_messages_id():
     return "hello messages id"
 
+
 @app.route("/api/admin/messages", methods=['POST'])
 def post_messages():
     return "hello post messages"
 
+
 @app.route("/api/admin/messages/:id", methods=['PUT'])
 def put_messages_id():
     return "hello put messages id"
+
 
 @app.route("/api/admin/messages/:id", methods=['DELETE'])
 def delete_messages_id():
