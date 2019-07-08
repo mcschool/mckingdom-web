3eb6d908bef77e76a1d238a3b77283f4a44cdc10
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-08 15:28:31 +0900

    feature/#22

--- server/app/app.py
+++ server/app/app.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 from sqlalchemy.orm import sessionmaker
 from .db import get_database_engine
 
-from app.api.game import player as game_player, access as game_access
-from app.api.admin import player as admin_player, access as admin_access, message as admin_message
+from app.api.game import player as game_player, access as game_access, world as game_world
+from app.api.admin import player as admin_player, access as admin_access, message as admin_message, world as admin_world
 
 application = Flask(__name__)
 apps = [
@@ -12,7 +12,9 @@ apps = [
     game_access.app,
     admin_player.app,
     admin_access.app,
-    admin_message.app
+    admin_message.app,
+    admin_world.app,
+    game_world.app
 ]
 for app in apps:
     application.register_blueprint(app)
