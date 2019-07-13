b4b315249f5d37170f57b941b049f305ccb45355
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-13 15:41:41 +0900

    final

--- server/app/api/game/command.py
+++ server/app/api/game/command.py
@@ -6,7 +6,7 @@ app = Blueprint('game_command', __name__)
 
 
 @app.route("/api/game/command/password", methods=['PUT'])
-def put_command():
+def put_command_password():
     data = request.get_json()
     if data.get('uuid') is None:
         return "error: no uuid"
@@ -19,4 +19,31 @@ def put_command():
             ).first()
             player.password = data.get('password')
             g.session.commit()
+    return "success"
+
+
+@app.route("/api/game/command/email", methods=['PUT'])
+def put_command_email():
+    data = request.get_json()
+    if data.get('uuid') is None:
+        return "error: no uuid"
+    if data.get('email') is None:
+        return "error: no email"
+    print(data.get('email'))
+    if len(data.get('email')) >= 10:
+        if len(data.get('email')) <= 240:
+            print("=====")
+            print(data.get('email'))
+            if '@' in data.get('email'):
+                print("=====")
+                print("in @ mark if")
+                player = g.session.query(Player).filter(
+                    Player.uuid == data.get('uuid')
+                ).first()
+                player.email = data.get('email')
+                g.session.commit()
+                print("=====")
+                print("end of if")
+            else:
+                return "error: no @ mark"
     return "success"
