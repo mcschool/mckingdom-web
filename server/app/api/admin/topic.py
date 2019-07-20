8f5a4be526467de4f0e24cbec6b0467faab7f2bd
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-20 15:28:17 +0900

    feature/#58

--- server/app/api/admin/topic.py
+++ server/app/api/admin/topic.py
@@ -1,4 +1,40 @@
 from flask import Blueprint, g, request, jsonify
-
+from datetime import datetime
+from ...models.topic import Topic
 
 app = Blueprint('admin_topic', __name__)
+
+
+@app.route("/api/admin/topics", methods=['POST'])
+def post_topics():
+    data = request.get_json()
+    if data.get('title') is None:
+        return"error: no title"
+    topic = Topic()
+    topic.title = data.get('title')
+    topic.body = data.get('body')
+    topic.is_published = data.get('is_published')
+    print("=====")
+    print(topic.is_published)
+    print("=====")
+    if topic.is_published == "true":
+        topic.published_at = datetime.now()
+        print("inside if")
+        print("=====")
+    else:
+        topic.published_at = None
+    g.session.add(topic)
+    g.session.commit()
+    return "success"
+
+
+@app.route("/api/admin/topics", methods=['GET'])
+def get_topics():
+    topics = g.session.query(Topic).all()
+    topics_data = []
+    for topic in topics:
+        topics_data.append(topic.as_dict())
+    data = {
+        "topics": topics_data
+    }
+    return jsonify(data)
