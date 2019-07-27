32f03925e87addb08b2c3402e5e79c6d93daaf03
Kensei Yoshida <ken_kun1031@Kenseis-MacBook-Pro.local>
2019-07-27 14:57:59 +0900

    feature/#63

--- server/app/api/admin/access.py
+++ server/app/api/admin/access.py
@@ -1,12 +1,13 @@
 import json
 from flask import Blueprint, g, request, jsonify
+from sqlalchemy import desc
 from ...models.access import Access
 
 app = Blueprint('admin_access', __name__)
 
 
 @app.route("/api/admin/access/game", methods=['GET'])
-def get_accesses():
+def get_accesses_game():
     accesses = g.session.query(Access).all()
     response = []
     for access in accesses:
@@ -15,10 +16,19 @@ def get_accesses():
 
 
 @app.route("/api/admin/access/game/<id>", methods=['GET'])
-def post_accesses(id = None):
+def post_accesses_game_id(id = None):
     print(id)
     access =g.session.query(Access).filter(
         Access.id == id
     ).first()
     response = access.as_dict()
+    return jsonify(response)
+
+
+@app.route("/api/admin/access", methods = ['GET'])
+def get_access():
+    accesses = g.session.query(Access).order_by(desc(Access.id)).all()
+    response = []
+    for access in accesses:
+        response.append(access.as_dict())
     return jsonify(response)
