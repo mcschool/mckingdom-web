67c452d922b73e5bf933dd4df4494614834b29d5
Yuki-Shibanuma <smatosan@gmail.com>
2019-08-03 09:01:36 +0900

    c

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 # docker build
 docker-compose build --no-cache web
 docker-compose build --no-cache server
+docker-compose build --no-cache nginx
 
 
 # サーバー再起動
@@ -11,8 +12,12 @@ docker-compose up -d server
 docker stop mckingdom-web_web_1
 docker-compose up -d web
 
+# nginx再起動
+docker stop mckingdom-web_nginx_1
+docker-compose up -d nginx
+
 # 余計なコンテナ削除
-docker container prune
+docker container prune -f
 
 # 余計なイメージ削除
 docker image prune -f
