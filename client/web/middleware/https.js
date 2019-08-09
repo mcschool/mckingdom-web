9916eda59c7e278d1055b7956fbb291ac020860b
Yuki-Shibanuma <smatosan@gmail.com>
2019-08-10 08:56:42 +0900

    a

--- client/web/middleware/https.js
+++ client/web/middleware/https.js
@@ -1,8 +1,10 @@
 export default ({ req, redirect }) => {
+  /*
   if (process.env.NODE_ENV === "production") {
     const protocol = req.protocol
     if (protocol === "http") {
       return redirect(301, `https://mc-kingdom.com/${req.url}`)
     }
   }
+  */
 }
