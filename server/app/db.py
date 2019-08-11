ec956d8b94c2db79677548557baf836c012d4d56
Yuki-Shibanuma <smatosan@gmail.com>
2019-08-11 14:17:52 +0900

    a

--- server/app/db.py
+++ server/app/db.py
@@ -5,10 +5,6 @@ from pprint import pprint as p
 
 
 def get_database_engine():
-    print("*******************")
-    p(current_app.config)
-    print("*******************")
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@db/mckingdom'  # current_app.config['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_DATABASE_URI = current_app.config['SQLALCHEMY_DATABASE_URI']
     DEBUG = current_app.config['DEBUG']
     return sqlalchemy.create_engine(
